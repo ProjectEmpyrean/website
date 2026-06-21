c7fc0f6680e694bf8c1c8aab428b72712264beb6
sublimode <shashank@projectempyrean.org>
2026-06-21 17:51:07 -0400

    feat: center text

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,12 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="p-10 h-dvh flex flex-col justify-center">
-      <div>
+    <main className="p-10 h-dvh flex flex-col justify-center items-center">
+      <span>
         This website is under construction. Visit the{" "}
         <a href="http://garden.projectempyrean.org">Garden</a> to learn more
         about Project Empyrean.
-      </div>
+      </span>
     </main>
   );
 }
